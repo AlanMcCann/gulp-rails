4e9ede85babba6b065fa07afdcba5d900c6099a4
Alan McCann <alan@imccann.com>
2017-02-20 16:41:46 -0500

    Removing duplication of asset type in path.
    The rev-manifest.json file requires searching with the asset type as part of the key. It returns a path that then duplicates itself in the url returned by the standard helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,30 @@
 module ApplicationHelper
   def stylesheet_link_tag(url, options={})
-    url = GulpRails::AssetManifest.stylesheet_path(url)
+    url = GulpRails::AssetManifest.stylesheet_path(url).sub('stylesheets/','')
 
     super(url, options)
   end
 
   def crossorigin_javascript_include_tag(url, options={})
-    url = GulpRails::AssetManifest.javascript_path(url)
+    url = GulpRails::AssetManifest.javascript_path(url).sub('javascripts/','')
 
     super(url, options)
   end
 
   def image_tag(url, options={})
-    url = GulpRails::AssetManifest.asset_path(url)
+    url = GulpRails::AssetManifest.asset_path(url).sub('images/','')
 
     super(url, options)
   end
 
   def image_path(url, options={})
-    url = GulpRails::AssetManifest.asset_path(url)
+    url = GulpRails::AssetManifest.asset_path(url).sub('images/','')
 
     super(url, options)
   end
 
   def image_url(url, options={})
-    url = GulpRails::AssetManifest.asset_path(url)
+    url = GulpRails::AssetManifest.asset_path(url).sub('images/','')
 
     super((ActionController::Base.asset_host || "") + url, options)
   end
